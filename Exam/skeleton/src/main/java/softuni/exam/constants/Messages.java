cbfd71ecb772a23d929999d873337e65930fb871
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-01-17 22:38:11 +0000

    Add files via upload

---
+++ Exam/skeleton/src/main/java/softuni/exam/constants/Messages.java
@@ -0,0 +1,21 @@
+package softuni.exam.constants;
+
+public class Messages {
+
+    public static String SUCCESSFULLY_IMPORTED_PART = "Successfully imported part %s - %.2f%n";
+    public static String INVALID_PART = "Invalid part";
+
+    public static String SUCCESSFULLY_IMPORTED_MECHANIC = "Successfully imported mechanic %s %s%n";
+    public static String INVALID_MECHANIC = "Invalid mechanic";
+
+    public static String SUCCESSFULLY_IMPORTED_CAR = "Successfully imported car %s - %s%n";
+    public static String INVALID_CAR = "Invalid car";
+
+    public static String SUCCESSFULLY_IMPORTED_TASK = "Successfully imported task %.2f%n";
+    public static String INVALID_TASK = "Invalid task";
+
+    public static String OUTPUT = "Car %s %s with %dkm%n" +
+            "              -Mechanic: %s %s - task №%d:%n" +
+            "              --Engine: %.1f%n" +
+            "              ---Price: %.2f$";
+}
