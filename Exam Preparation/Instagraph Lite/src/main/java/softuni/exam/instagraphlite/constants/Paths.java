1feb3b1fa33808df6e8319cb5d3458e9e1ffa59d
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-01-20 21:57:39 +0000

    Add files via upload

---
+++ Exam Preparation/Instagraph Lite/src/main/java/softuni/exam/instagraphlite/constants/Paths.java
@@ -0,0 +1,10 @@
+package softuni.exam.instagraphlite.constants;
+
+import java.nio.file.Path;
+
+public enum Paths {
+    ;
+    public static Path PICTURES_IMPORT_JSON = Path.of("src/main/resources/files/pictures.json");
+    public static Path USERS_IMPORT_JSON = Path.of("src/main/resources/files/users.json");
+    public static Path POSTS_IMPORT_XML = Path.of("src/main/resources/files/posts.xml");
+}
