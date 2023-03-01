9192db95f7fbf566c820e754867826e63433d694
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-03-01 23:52:06 +0000

    Add files via upload

---
+++ DB Apps Introduction - Exercise/src/Utils.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Utils {
+    public static Connection getConnection() throws SQLException {
+        Properties props = new Properties();
+        props.setProperty("user", "root");
+        props.setProperty("password", "");
+
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", props);
+    }
+}
