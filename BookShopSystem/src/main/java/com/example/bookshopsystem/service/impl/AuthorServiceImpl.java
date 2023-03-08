347dd080fd30c5a9919a6227442c209ffbc86a21
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-03-08 23:11:23 +0000

    Add files via upload

---
+++ BookShopSystem/src/main/java/com/example/bookshopsystem/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,75 @@
+package com.example.bookshopsystem.service.impl;
+
+import com.example.bookshopsystem.entities.Author;
+import com.example.bookshopsystem.repository.AuthorRepository;
+import com.example.bookshopsystem.service.AuthorService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    private static final String AUTHORS_FILE_PATH = "src/main/resources/files/authors.txt";
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public void seedAuthors() throws IOException {
+        if (authorRepository.count() > 0) {
+            return;
+        }
+
+        Files
+                .readAllLines(Path.of(AUTHORS_FILE_PATH))
+                .forEach(row -> {
+                    String[] fullName = row.split("\\s+");
+                    Author author = new Author(fullName[0], fullName[1]);
+
+                    authorRepository.save(author);
+                });
+    }
+
+    @Override
+    public Author getRandomAuthor() {
+        long randomId = ThreadLocalRandom
+                .current().nextLong(1,
+                        authorRepository.count() + 1);
+
+        return authorRepository
+                .findById(randomId)
+                .orElse(null);
+    }
+
+    @Override
+    public List<String> getAllAuthorsOrderByCountOfTheirBooks() {
+        return authorRepository
+                .findAllByBooksSizeDESC()
+                .stream()
+                .map(author -> String.format("%s %s %d",
+                        author.getFirstName(),
+                        author.getLastName(),
+                        author.getBooks().size()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Author> findAllByFirstNameEndingWith(String letterToEndWith) {
+        List<Author> authors = this.authorRepository.findAllByFirstNameEndingWith(letterToEndWith).orElseThrow(NoSuchElementException::new);
+
+        authors.stream().map(Author::firstAndLastName).forEach(System.out::println);
+        return authors;
+    }
+
+
+}
